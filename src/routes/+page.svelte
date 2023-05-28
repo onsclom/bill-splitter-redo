96c20f20d9413a72836116ed53d264c9e4bc201c
onsclom <onsclom@onsclom.net>
2023-05-27 20:00:02 -0700

    Various UX improvements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,7 +18,6 @@
 	];
 
 	let personNumber = people.length;
-
 	let subtotal = 100;
 
 	// TODO: implement two way bindings on these
@@ -47,68 +46,64 @@
 
 <h1>bill splitter 🧾 💵</h1>
 
-<section>
-	<h2>people</h2>
-	{#each people as person}
-		<div style="display: flex; gap: 1rem; align-items: center;">
-			<input style="flex-grow: 1" bind:this={person.input} bind:value={person.name} />
-			<button on:click={() => person.input?.select()}>✏️</button>
-			<button on:click={() => (people = people.filter((p) => p !== person))}> ❌ </button>
-		</div>
-	{/each}
-	<button
-		style="text-align: center"
-		on:click={() =>
-			(people = [
-				...people,
-				{ name: `Person ${++personNumber}`, id: crypto.randomUUID(), input: null }
-			])}
-	>
-		+ 😃
-	</button>
-</section>
-
-<section>
-	<h2>totals</h2>
-	<p>
-		<label>
-			subtotal $<input type="number" bind:value={subtotal} />
-		</label>
-	</p>
-
-	<div style="display: flex; gap: 1rem; align-items: center">
-		<label>
-			tax %<input type="number" autocomplete="off" bind:value={taxPercentage} />
-		</label>
-		<span> = </span>
-		<label>
-			tax $<input type="number" autocomplete="off" bind:value={tax} disabled />
-		</label>
+<h2>people</h2>
+{#each people as person}
+	<div style="display: flex; gap: 1rem; align-items: center;">
+		<input style="flex-grow: 1" bind:this={person.input} bind:value={person.name} />
+		<button on:click={() => person.input?.select()}>✏️</button>
+		<button on:click={() => (people = people.filter((p) => p !== person))}> ❌ </button>
 	</div>
+{/each}
+<button
+	style="text-align: center"
+	on:click={() =>
+		(people = [
+			...people,
+			{ name: `Person ${++personNumber}`, id: crypto.randomUUID(), input: null }
+		])}
+>
+	+ 😃
+</button>
 
-	<div style="display: flex; gap: 1rem; align-items: center">
-		<label>
-			tip %<input type="number" autocomplete="off" bind:value={tipPercentage} />
-		</label>
-		<span> = </span>
-		<label>
-			tip $<input type="number" autocomplete="off" bind:value={tip} disabled />
-		</label>
-	</div>
+<h2>totals</h2>
+<p>
+	<label>
+		subtotal $<input inputmode="decimal" type="number" bind:value={subtotal} />
+	</label>
+</p>
 
-	<p>
-		total <strong>
-			${(subtotal + tax + tip).toFixed(2)}
-		</strong>
-	</p>
-</section>
+<div style="display: flex; gap: 1rem; align-items: center">
+	<label>
+		tax %<input type="number" inputmode="decimal" autocomplete="off" bind:value={taxPercentage} />
+	</label>
+	<span> = </span>
+	<label>
+		tax $<input type="number" inputmode="decimal" autocomplete="off" bind:value={tax} disabled />
+	</label>
+</div>
 
-<section>
-	<h2>items</h2>
+<div style="display: flex; gap: 1rem; align-items: center">
+	<label>
+		tip %<input type="number" inputmode="decimal" autocomplete="off" bind:value={tipPercentage} />
+	</label>
+	<span> = </span>
+	<label>
+		tip $<input type="number" inputmode="decimal" autocomplete="off" bind:value={tip} disabled />
+	</label>
+</div>
 
-	{#each items as item}
+<p>
+	total <strong>
+		${(subtotal + tax + tip).toFixed(2)}
+	</strong>
+</p>
+
+<h2>items</h2>
+
+{#each items as item}
+	<div style="display: flex; align-items: center; gap: 1rem">
+		<button on:click={() => (items = items.filter((i) => i !== item))}> ❌ </button>
 		<div>
-			<button on:click={() => (items = items.filter((i) => i !== item))}> ❌ </button>
 			<strong>{item.name}</strong> -
 			<strong>${item.value}</strong>
 			{item.payers.length > 1 ? ' split between ' : ' paid by '}
@@ -116,90 +111,94 @@
 				{item.payers.map((payer) => people.find((person) => person.id === payer)?.name).join(', ')}
 			</strong>
 		</div>
-	{/each}
+	</div>
+{/each}
 
-	<br />
-
-	<div>
-		<label>name <input type="text" bind:value={itemName} /></label>
-		<label>price $<input type="number" bind:value={itemValue} /></label>
-		<label>
-			payers <br />
+<div>
+	<div style="display: flex; gap: 1rem;">
+		<label>name <input style="flex-grow: 1" type="text" bind:value={itemName} /></label>
+		<label
+			>price $<input
+				style="flex-grow: 1"
+				inputmode="decimal"
+				type="number"
+				bind:value={itemValue}
+			/></label
+		>
+	</div>
+	<label>
+		paid by <br />
+		<div style="display: flex; gap: 1rem; flex-wrap: wrap">
 			{#each people as person}
-				<label>
+				<label style="white-space: nowrap">
 					<input type="checkbox" bind:group={itemPayers} value={person.id} />
 					{person.name}
-					{itemPayers.includes(person.id)
-						? `($${Number((itemValue / itemPayers.length).toFixed(2))})`
-						: ''}
 				</label>
 			{/each}
-		</label>
-		<button
-			on:click={() => {
-				items = [
-					...items,
-					{ name: itemName, value: itemValue, payers: itemPayers, id: crypto.randomUUID() }
-				];
-				itemName = `item ${++itemNumber}`;
-				itemValue = 10;
-				itemPayers = [];
-			}}
-			disabled={itemPayers.length === 0}
-		>
-			+ item
-		</button>
-	</div>
-</section>
+		</div>
+	</label>
+	<button
+		on:click={() => {
+			items = [
+				...items,
+				{ name: itemName, value: itemValue, payers: itemPayers, id: crypto.randomUUID() }
+			];
+			itemName = `item ${++itemNumber}`;
+			itemValue = 10;
+			itemPayers = [];
+		}}
+		disabled={itemPayers.length === 0}
+	>
+		+ item
+	</button>
+</div>
 
-<section>
-	<h2>results</h2>
+<h2>results</h2>
 
-	{#if itemsMatchSubtotal}
-		<p>✅ items match subtotal</p>
-		<table>
-			<thead>
+{#if itemsMatchSubtotal}
+	<p>✅ items match subtotal</p>
+	<table>
+		<thead>
+			<tr>
+				<th>name</th>
+				<th>subtotal</th>
+				<th>total</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each people as person}
 				<tr>
-					<th>name</th>
-					<th>subtotal</th>
-					<th>total</th>
+					<td>{person.name}</td>
+					<td
+						>${items
+							.filter((item) => item.payers.includes(person.id))
+							.reduce((sum, item) => sum + item.value / item.payers.length, 0)
+							.toFixed(2)}</td
+					>
+					<td>
+						<strong>
+							${(
+								items
+									.filter((item) => item.payers.includes(person.id))
+									.reduce((sum, item) => sum + item.value / item.payers.length, 0) *
+								(1 + taxPercentage / 100 + tipPercentage / 100)
+							).toFixed(2)}
+						</strong>
+					</td>
 				</tr>
-			</thead>
-			<tbody>
-				{#each people as person}
-					<tr>
-						<td>{person.name}</td>
-						<td
-							>${items
-								.filter((item) => item.payers.includes(person.id))
-								.reduce((sum, item) => sum + item.value / item.payers.length, 0)
-								.toFixed(2)}</td
-						>
-						<td>
-							<strong>
-								${(
-									items
-										.filter((item) => item.payers.includes(person.id))
-										.reduce((sum, item) => sum + item.value / item.payers.length, 0) *
-									(1 + taxPercentage / 100 + tipPercentage / 100)
-								).toFixed(2)}
-							</strong>
-						</td>
-					</tr>
-				{/each}
-			</tbody>
-		</table>
-	{:else if subtotal > itemSum}
-		<p>❌ items are less than subtotal</p>
-		<p>
-			there should be
-			<strong>${(subtotal - itemSum).toFixed(2)}</strong> more of items
-		</p>
-	{:else}
-		<p>❌ items are more than subtotal</p>
-		<p>
-			there should be
-			<strong>${(itemSum - subtotal).toFixed(2)}</strong> less of items
-		</p>
-	{/if}
-</section>
+			{/each}
+		</tbody>
+	</table>
+{:else if subtotal > itemSum}
+	<p>❌ items are less than subtotal</p>
+	<p>
+		there should be
+		<strong>${(subtotal - itemSum).toFixed(2)}</strong> more of items
+	</p>
+{:else}
+	<p>❌ items are more than subtotal</p>
+	<p>
+		there should be
+		<strong>${(itemSum - subtotal).toFixed(2)}</strong> less of items
+	</p>
+{/if}
